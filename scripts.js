49692834c3aadf9fef44b3dfaed069d9894bdc8f
Maciel Lima <maciel.lima.souza02@aluno.ifce.edu.br>
2025-09-13 23:49:45 -0300

    commit inicial

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector('.h1')
+
+setInterval(()=>{{
+    h1.textContent = 'Meu Brasil é do Lula!'
+}}, 1000)
+
+
+setInterval(()=>{{
+    h1.textContent = 'Meu Brasil é corinthiano!'
+}}, 2000)
